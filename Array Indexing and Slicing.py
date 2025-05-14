0722605ab8ae6904f7a4c42beebd7388fd694e21
MSAA <msaa131911@gmail.com>
2025-05-14 23:31:54 +0600

    Add files via upload

---
+++ Array Indexing and Slicing.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#arr1=np.array([1,2,3,4,5,6,7,8,9])
+#print(arr1[-2])
+
+#Fsrr2=np.random.randint(1,10,[3,3])
+
+#print(srr2[0][0],srr2)
+#arr3=np.random.randint(1,10,[2,3,3])
+#print(arr3[1][1][2],arr3)
+
+#Slicing
+arr4=np.random.randint(1,10,[3,3])
+print(arr4)
+print(arr4[0:2,0:2])
