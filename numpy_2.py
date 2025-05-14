1541baa29166558d3334511fa9906d511aba80e9
MSAA <msaa131911@gmail.com>
2025-05-14 21:44:36 +0600

    Add files via upload

---
+++ numpy_2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+dd=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(dd)
+print(dd.shape)
+print(dd.ndim)
+print(dd.size)
+print(dd.dtype)
+#print(dd.itemsize)
+print(dd.data)
+print(dd.nbytes)
