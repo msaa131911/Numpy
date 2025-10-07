e46878ab97f301b01a787c4dbdda0b6d9f776389
MSAA <msaa131911@gmail.com>
2025-10-07 21:38:51 +0600

    Add files via upload

---
+++ frompyfunc.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def myadd(x,y):
+    return x+y
+
+ss=np.frompyfunc(myadd,2,1) #frompyfunc(function,inputs,outputs)
+print(ss([1,2,3,4],[5,6,7,8]))
+
+
+#Check if a Function is a ufunc
+print(type(np.add))
